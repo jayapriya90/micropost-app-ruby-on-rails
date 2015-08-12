302dbe99112fde40d82228fc1cece63cc4923855
jayapriya90 <priya7390@gmail.com>
2015-08-12 10:39:57 +0000

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # presence: true is one element options hash. curly braces is optional in Rails if hash is last argument)
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
