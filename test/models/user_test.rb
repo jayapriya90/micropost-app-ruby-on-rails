9cad8c928591c2a13a6c1d7aa1746e42e3d61456
jayapriya90 <priya7390@gmail.com>
2015-10-01 23:15:29 +0000

    Add micropost status feed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -89,4 +89,22 @@ class UserTest < ActiveSupport::TestCase
     priya.unfollow(archer)
     assert_not priya.following?(archer)
   end
+  
+  test "feed should have the right posts" do
+    priya = users(:priya)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    #lana.microposts.each do |post_following|
+    #  assert priya.feed.include?(post_following)
+    #end
+    # Posts from self
+    priya.microposts.each do |post_self|
+      assert priya.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not priya.feed.include?(post_unfollowed)
+    end
+  end
 end
